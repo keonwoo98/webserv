59b9135da7ac8720765a939a4d21a7920b5d0fbd
Minjune <minjune8506@gmail.com>
2022-12-01 19:26:41 +0900

    feat: Delete Method 기능 구현
    - 파일 삭제 기능

---
+++ sources/delete_method.cpp
@@ -0,0 +1,37 @@
+//
+// Created by 김민준 on 2022/12/01.
+//
+
+/**
+ * DELETE Method
+ *
+ * Directory 삭제 시도 -> 409 Conflict
+ * 없는 파일 삭제 시도 -> 404 Not Found
+ */
+#include "delete_method.hpp"
+#include "status_code.hpp"
+
+#include <string>
+#include <fcntl.h>
+#include <iostream>
+#include <unistd.h>
+
+
+int DeleteMethod(const std::string &uri) {
+	char path[128];
+	getcwd(path, 128);
+	std::cerr << path << std::endl;
+	int fd = open(uri.c_str(), O_RDWR);
+	if (fd < 0) {
+		std::perror("open: ");
+		if (errno == ENOENT) {
+			return NOT_FOUND;
+		}
+		return CONFLICT;
+	}
+	if (unlink(uri.c_str()) < 0) {
+		std::perror("unlink: ");
+		return CONFLICT;
+	}
+	return OK;
+}
