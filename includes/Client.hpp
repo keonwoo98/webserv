5de41c911390d86095535973af4fbd0056dbee53
42dkim2 <ulol@42dkim2ui-MacBookPro.local>
2022-12-06 00:12:53 +0900

    chore : clear

--- includes/Client.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef CLIENT_HPP
-#define CLIENT_HPP
-
-#include "Server.hpp"
-
-#endif
--- includes/Server.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef SERVER_HPP
-#define SERVER_HPP
-
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <fcntl.h>
-
-#endif
