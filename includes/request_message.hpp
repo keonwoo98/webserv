c0c2d08fa3493f5fc2cde73c9caeeb4cd47c68ea
u-lo-l <jeffk0408@naver.com>
2022-12-08 02:29:39 +0900

    feat(Request) : add chunked request parser

--- includes/request_message.hpp
+++ includes/request_message.hpp
@@ -22,6 +22,7 @@ class RequestMessage {
 	const std::string &GetMethod() const;
 	const std::string &GetUri() const;
 	const header_map_type &GetHeaderMap() const;
+	const std::string &GetBody() const;
 
 	void ParsingMessage(const std::string &message);
 
@@ -30,10 +31,57 @@ class RequestMessage {
 	std::string uri_;
 	header_map_type header_map_;
 	std::string body_;
-	size_t	content_size_;
+	int		content_size_;
 	bool	is_chunked_;
 	bool	keep_alive_;
 
+	/* switch 문으로 case by case로 파싱하다보니 이렇게도 가능 할 것 같아서 chunked body class하나 만들어 보겠습니다. */
+	class RequestChunkedMessage {
+	   private :
+		enum state {
+			CHUNK_START = 0,
+			CHUNK_SIZE, CHUNK_SIZE_CRLF,
+			CHUNK_EXTENSION, CHUNK_EXTENSION_NAME, CHUNK_EXTENSION_VALUE,
+			CHUNK_DATA, CHUNK_LASTDATA, CHUNK_CRLF,
+			CHUNK_TRAILER, CHUNK_TRAILER_NAME, CHUNK_TRAILER_VALUE, CHUNK_TRAILER_CRLF,
+			CHUNK_EMPTYLINE, CHUNK_ERROR,
+			CHUNK_END,
+		};
+		bool last_chunk_;
+		size_t chunk_size_;
+		std::string chunk_size_str_;
+		std::string chunk_body_;
+		std::string error_msg_;
+	   public :
+		RequestChunkedMessage();
+		~RequestChunkedMessage();
+		std::string operator() (const char * str);
+		// const std::string & GetBody() const { return chunk_body_; }
+		// void ClearBody() { chunk_body_.clear(); }
+	   private :
+		state (RequestMessage::RequestChunkedMessage::*parse_state_[15])(char);
+		state ParseChunk(state curr_state);
+		state ChunkStart(char c);
+		state ChunkSize(char c);
+		state ChunkSizeCRLF(char c);
+
+		state ChunkExtension(char c);
+		state ChunkExtensionName(char c);
+		state ChunkExtensionValue(char c);
+		
+		state ChunkData(char c);
+		state ChunkLastData(char c);
+		state ChunkCRLF(char c);
+		
+		state ChunkTrailer(char c);
+		state ChunkTrailerName(char c);
+		state ChunkTrailerValue(char c);
+		state ChunkTrailerCRLF(char c);
+		
+		state ChunkEmptyLine(char c);
+		state Chunk_Error(char c);
+	};
+	RequestChunkedMessage chunk_parser_functor_;
 
 	std::set<std::string> valid_header_name_;
 	std::set<std::string> valid_header_value_;
@@ -41,6 +89,7 @@ class RequestMessage {
 	void ParsingStartLine(const std::string start_line);
 	void ParsingHeaders(const std::string headers);
 	void SplitHeader(const std::string &header);
+	bool CheckHeader(void) const;
 	void ParsingBody(const std::string &body);
 
 	bool isTchar(char c);
--- sources/request_message.cpp
+++ sources/request_message.cpp
@@ -3,18 +3,9 @@
 #include <fstream>
 #include <locale>
 
-RequestMessage::RequestMessage() {
-	std::string filePath = "ex/request-headers";
-
-	std::ifstream openFile(filePath.data());
-	if (openFile.is_open()) {
-		std::string line;
-		while (getline(openFile, line)) {
-			valid_header_name_.insert(line);
-		}
-		openFile.close();
-	}
-}
+RequestMessage::RequestMessage()
+: content_size_(-1), is_chunked_(false), keep_alive_(true)
+{}
 
 RequestMessage::~RequestMessage() {}
 
@@ -30,145 +21,77 @@ const RequestMessage::header_map_type &RequestMessage::GetHeaderMap() const {
 	return header_map_;
 }
 
+const std::string &RequestMessage::GetBody() const {
+	return body_;
+}
+
 void RequestMessage::ParsingMessage(const std::string &message) {
 	std::string buf;
 	size_t index;
 
-	index = message.find("R\n");
+	index = message.find(CRLF);
 	buf = message.substr(0, index + 1);
 	ParsingStartLine(buf);
 
-	index = message.find("R\nR\n");
+	index = message.find(DCRLF);
 	buf = message.substr(buf.length(), index- buf.length() + 2);
 	ParsingHeaders(buf);
 
-	body_ = message.substr(index + 4, message.length() - (index + 4));
+	buf = message.substr(index + 4, message.length() - (index + 4));
+	body_ += this->chunk_parser_functor_(buf.c_str());
 }
 
 void RequestMessage::ParsingStartLine(const std::string start_line) {
-	std::stringstream ss(start_line);
-	std::string buf;
+	std::stringstream start_line_stream(start_line);
+	std::string protocol;
 
-	std::getline(ss, method_, ' ');
+	std::getline(start_line_stream, method_, SP);
 	if (method_ != "GET" && method_ != "POST" && method_ != "DELETE")
 		std::cerr << "error: " << method_ << std::endl;
 
-	std::getline(ss, uri_, ' ');
+	std::getline(start_line_stream, uri_, SP);
 
-	std::getline(ss, buf, 'R');
-	if (buf != "HTTP/1.1")
-		std::cerr << "error: " << buf << std::endl;
-}
-
-void IsValidHeaderName(std::string name, std::string value) {
-	
+	std::getline(start_line_stream, protocol, CR);
+	if (protocol != "HTTP/1.1")
+		std::cerr << "error: " << protocol << std::endl;
 }
 
 void RequestMessage::SplitHeader(const std::string &header) {
 	std::string name;
 	std::string value;
-	size_t index;
+	std::string::size_type colon_pos;
 
-	index = header.find(": ");
-	name = header.substr(0, index);
-	value = header.substr(index + 2, header.length());
+	colon_pos = header.find(":");
+	name = header.substr(0, colon_pos);
+	if (header[colon_pos + 1] != SP)
+	{
+		std::cerr << "error: <request header> No SP after colon" << method_ << std::endl;
+		return ;
+	}
+	value = header.substr(colon_pos + 2, header.length());
 	header_map_[name] = value;
+}
+
+bool RequestMessage::CheckHeader(void) const {
+	return true;
 }
 
 void RequestMessage::ParsingHeaders(const std::string headers) {
 	size_t start = 0;
 	size_t end;
 
-	end = headers.find("R\n");
-	size_t i = 0;
+	end = headers.find(CRLF, 0);
 	while (end != std::string::npos) {
 		SplitHeader(headers.substr(start, end - start));
 		start = end + 2;
-		end = headers.find("R\n", start);
+		end = headers.find(CRLF, start);
 	}
 }
 
 /*여기서 body에 넘어오는 문자열은 헤더와 바디를 구분하는 CRLF다음부터 메시지의 끝까지이다.*/
 void RequestMessage::ParsingBody(const std::string &message_body)
 {
-	if (is_chunked_ == false)
-	{
-		this->body_.reserve(content_size_);
-		this->body_.assign(message_body, 0, content_size_);
-	}
-	else
-	{
-		enum chunk_state
-		{ 
-			CHUNK_SIZE = 0, 
-			CHUNK_EXTENSION,
-			CHUNK_SIZE_CRLF,
-			CHUNK_BODY,
-			CHUNK_LASTBODY,
-			CHUNK_CRLF,
-			CHUNK_CRLF2,
-			CHUNK_TRAILER,
-			CHUNK_END
-		};
-
-		chunk_state state = CHUNK_SIZE;
-		size_t chunk_size = 0;
-		std::string chunk_size_str = "";
-		for (int i = 0 ; state != CHUNK_END ; i++)
-		{
-			switch (state)
-			{
-				case CHUNK_SIZE:
-					chunk_size = 0;
-					while (std::isdigit(message_body[i]))
-						chunk_size_str.push_back(message_body[i++]);
-					chunk_size = std::atoi(chunk_size_str.c_str());
-					state = (message_body[i] == 'R') ? CHUNK_SIZE_CRLF : CHUNK_EXTENSION;
-					break;
-
-				case CHUNK_EXTENSION:
-					while (message_body[i] != 'R')
-						i++;
-					state = CHUNK_SIZE_CRLF;
-					break;
-				
-				case CHUNK_SIZE_CRLF:
-					if (message_body[i] != '\n')
-						return ; // ERROR
-					state = (chunk_size == 0) ? CHUNK_LASTBODY : CHUNK_BODY;
-					break;
-				
-				case CHUNK_BODY:
-					while (std::isdigit(message_body[i]))
-						chunk_size_str.push_back(message_body[i++]);
-					state = (message_body[i] == 'R') ? CHUNK_CRLF : CHUNK_TRAILER;
-					break;
-				
-				case CHUNK_LASTBODY:
-					break;
-				
-				case CHUNK_TRAILER:
-					while (message_body[i] != 'R')
-						i++;
-					state = CHUNK_CRLF2;
-					break;
-				
-				case CHUNK_CRLF:
-				if (message_body[i] != '\n')
-					return ; // ERROR
-
-					break;
-
-				case CHUNK_CRLF2:
-					
-					break;
-				
-				default :
-					return ; //ERROR
-			}
-		}
-		std::cout << "body parse done" << std::endl;
-	}
+	size_t total_length = 0;
 
 }
 
