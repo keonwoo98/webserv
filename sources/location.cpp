98cd189e92159814d8f818ccdf3235bd79a4ad8f
maindishes <manynolan1@gmail.com>
2022-12-12 17:09:48 +0900

    chore(Conf Parser): set location root_ default html

--- sources/location.cpp
+++ sources/location.cpp
@@ -1,5 +1,5 @@
 #include "location.hpp"
 
-Location::Location() : client_max_body_size_(1000000), path_(""), root_("") {}
+Location::Location() : client_max_body_size_(1000000), path_(""), root_("./docs/index.html") {}
 
 Location::~Location() {}
