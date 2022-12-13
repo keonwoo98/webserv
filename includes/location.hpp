eb5403e5a59a5a12486d723570ff91483721bce6
maindishes <manynolan1@gmail.com>
2022-12-14 00:44:47 +0900

    refactor(not complete): add getter, setter

--- includes/location.hpp
+++ includes/location.hpp
@@ -6,7 +6,7 @@
 #include <map>
 
 class Location {
-   public:
+   private:
 	int client_max_body_size_;
 	std::string path_;
 	std::string root_;
@@ -16,6 +16,20 @@ class Location {
    public:
 	Location();
 	~Location();
+	const int &Location::GetClientMaxBodySize() const;
+	const std::string &Location::GetPath() const;
+	const std::string &Location::GetRoot() const;
+	const std::vector<std::string> &Location::GetIndex() const;
+	const std::vector<std::string> &Location::GetAllowMethods() const;
+	const std::vector<std::string> &Location::GetCgi() const;
+	
+	void Location::SetClientMaxBodySize(const int &client_max_body_size);
+	void Location::SetPath(const std::string &path);
+	void Location::SetRoot(const std::string &root);
+	void Location::SetIndex(const std::vector<std::string> &index);
+	void Location::SetAllowMethods(const std::string &allow_methods);
+	void Location::SetAllowMethods(const std::vector<std::string> &allow_methods);
+	void Location::SetCgi(const std::vector<std::string> &cgi);
 };
 
 #endif
--- sources/location.cpp
+++ sources/location.cpp
@@ -3,3 +3,55 @@
 Location::Location() : client_max_body_size_(1000000), path_(""), root_("./docs/index.html") {}
 
 Location::~Location() {}
+
+const int &Location::GetClientMaxBodySize() const {
+    return this->client_max_body_size_;
+}
+
+const std::string &Location::GetPath() const {
+    return this->path_;
+}
+
+const std::string &Location::GetRoot() const {
+    return this->root_;
+}
+
+const std::vector<std::string> &Location::GetIndex() const {
+    return this->index_;
+}
+
+const std::vector<std::string> &Location::GetAllowMethods() const {
+    return this->allow_methods_;
+}
+
+const std::vector<std::string> &Location::GetCgi() const {
+    return this->cgi_;
+}
+
+void Location::SetClientMaxBodySize(const int &client_max_body_size) {
+    this->client_max_body_size_ = client_max_body_size;
+}
+
+void Location::SetPath(const std::string &path) {
+    this->path_ = path;
+}
+
+void Location::SetRoot(const std::string &root) {
+    this->root_ = root;
+}
+
+void Location::SetIndex(const std::vector<std::string> &index) {
+    for (size_t i = 0; i < index.size(); i++)
+        this->index_.push_back(index[i]);
+    // this->index_ = index;
+}
+void Location::SetAllowMethods(const std::string &allow_methods) {
+    this->allow_methods_.push_back(allow_methods);
+}
+void Location::SetAllowMethods(const std::vector<std::string> &allow_methods) {
+    for (size_t i = 0; i < index.allow)
+}
+
+void Location::SetCgi(const std::vector<std::string> &cgi) {
+    this->cgi_ = cgi;
+}
