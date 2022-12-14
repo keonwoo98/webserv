086d0c315861f82f841db69ad7cbb99c0ebd4ceb
maindishes <manynolan1@gmail.com>
2022-12-14 13:35:59 +0900

    refactor: add getter, setter in cpp file

--- sources/location.cpp
+++ sources/location.cpp
@@ -40,16 +40,33 @@ void Location::SetRoot(const std::string &root) {
     this->root_ = root;
 }
 
+void Location::SetIndex(const std::string &index) {
+    this->index_.push_back(index);
+    // this->index_ = index;
+}
+
 void Location::SetIndex(const std::vector<std::string> &index) {
+    this->index_.clear();
     for (size_t i = 0; i < index.size(); i++)
         this->index_.push_back(index[i]);
     // this->index_ = index;
 }
+
+
 void Location::SetAllowMethods(const std::string &allow_methods) {
+    // this->allow_methods_.clear();
     this->allow_methods_.push_back(allow_methods);
 }
 void Location::SetAllowMethods(const std::vector<std::string> &allow_methods) {
-    for (size_t i = 0; i < index.allow)
+    this->allow_methods_.clear();
+    for (size_t i = 0; i < allow_methods.size(); i++)
+    {
+        this->allow_methods_.push_back(allow_methods[i]);
+    }
+}
+
+void Location::SetCgi(const std::string &cgi) {
+    this->cgi_.push_back(cgi);
 }
 
 void Location::SetCgi(const std::vector<std::string> &cgi) {
