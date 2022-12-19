a730d6e57b9522fcc2305985929927c1c30a06a4
u-lo-l <jeffk0408@naver.com>
2022-12-19 16:29:43 +0900

    feat: 문자 관련 상수 및 출력 문자 색 및 형태 관련 상수 추가

---
+++ includes/character_const/character_color.hpp
@@ -0,0 +1,23 @@
+#ifndef CHARACTER_COLOR_HPP
+# define CHARACTER_COLOR_HPP
+
+# define C_RESET	"\x1b[0m"
+/*COLOR*/
+# define C_BLACK	"\x1b[30m"
+# define C_RED		"\x1b[31m"
+# define C_GREEN	"\x1b[32m"
+# define C_YELLOW	"\x1b[33m"
+# define C_BLUE		"\x1b[34m"
+# define C_SKYBLUE	"\x1b[94m"
+# define C_PURPLE	"\x1b[35m"
+# define C_CYAN		"\x1b[36m"
+# define C_CYAN		"\x1b[96m"
+# define C_WHITE	"\x1b[37m"
+/**/
+# define C_BOLD			"\x1b[1m"
+# define C_FAINT		"\x1b[2m"
+# define C_FAINT		"\x1b[3m"
+# define C_UNDERLINE	"\x1b[4m"
+# define C_GOTHIC		"\x1b[20m"
+
+#endif
