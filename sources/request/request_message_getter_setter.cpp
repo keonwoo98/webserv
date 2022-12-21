d79b0a9de6e20c733b721264be1a01d50d69fbee
u-lo-l <jeffk0408@naver.com>
2022-12-22 03:56:13 +0900

    refactor: request Message 게터세터 파일분리

---
+++ sources/request/request_message_getter_setter.cpp
@@ -0,0 +1,91 @@
+#include "request_message.hpp"
+
+StatusCode	RequestMessage::GetStatusCode() const {
+	return (status_code_);
+}
+
+int	RequestMessage::GetContentSize() const {
+	return (content_size_);
+}
+
+bool	RequestMessage::IsChunked() const {
+	return (is_chunked_);
+}
+
+bool	RequestMessage::IsAlive() const {
+	return (keep_alive_);
+}
+
+RequestState	RequestMessage::GetState() const {
+	return (state_);
+}
+
+bool	RequestMessage::IsLastChunk() const {
+	return (last_chunk_flag_);
+}
+
+size_t	RequestMessage::GetChunkSize() const {
+	return (chunk_size_);
+}
+
+const std::string	&RequestMessage::GetChunkSizeStr() const {
+	return (chunk_size_str_);
+}
+
+const std::string	&RequestMessage::GetChunkBody() const {
+	return (chunk_body_);
+}
+
+const std::string	&RequestMessage::GetMethod() const {
+	return (method_);
+}
+
+const std::string	&RequestMessage::GetUri() const {
+	return (uri_);
+}
+
+const std::string	&RequestMessage::GetHttpVersion() const {
+	return (http_version_);
+}
+
+const RequestMessage::headers_type	&RequestMessage::GetHeaders() const {
+	return (headers_);
+}
+
+const std::string	&RequestMessage::GetBody() const {
+	return (body_);
+}
+
+
+void RequestMessage::SetState(RequestState code) {
+	state_ = code;
+}
+
+void RequestMessage::SetStatusCode(StatusCode code) {
+	status_code_ = code;
+}
+
+void RequestMessage::SetConnection(bool is_keep_alive) {
+	keep_alive_ = is_keep_alive;
+}
+
+void RequestMessage::SetLastChunk(bool is_last_chunk) {
+	last_chunk_flag_ = is_last_chunk;
+}
+
+void RequestMessage::SetChunkSize(size_t size) {
+	chunk_size_ = size;
+}
+
+void RequestMessage::ClearChunkSize() {
+	chunk_size_ = 0;
+}
+
+void RequestMessage::ClearChunkSizeStr() {
+	chunk_size_str_ = "";
+}
+
+void RequestMessage::ClaerChunkBody() {
+	chunk_body_ = "";
+}
+
