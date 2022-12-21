512898be566f03e4ea0745c4e13a27a1dd0b9c1f
u-lo-l <jeffk0408@naver.com>
2022-12-22 05:52:56 +0900

    chore: 파싱, 상태코드는 어느정도 됨. URI체크, 적절한 UDATA생성 및 이벤트 추가 하는 부분 추가되어야 함

--- sources/core/client_socket.cpp
+++ sources/core/client_socket.cpp
@@ -1,5 +1,6 @@
 #include "client_socket.hpp"
 #include "character_const.hpp"
+#include "request_parser.hpp"
 
 ClientSocket::ClientSocket(int sock_d, const ServerInfo &server_info)
   : Socket(server_info, Socket::CLIENT_TYPE, sock_d), 
@@ -20,6 +21,10 @@ void ClientSocket::RecvRequest() {
 		request_parser_(request_, tmp);
 	}
 	if (request_.GetState() == DONE) {
+		// 여기서 나머지, ServerInfo사용하는 체크 수행.
+		if (RequestStartlineCheck(request_, server_info_) == true){
+			RequestHeaderCheck(request_, server_info_);
+		}
 		std::cout << C_CYAN << "[ClientSocket::RecvRequest] : Parsing Done. result :" << C_RESET << std::endl;
 		std::cout << request_ << std::endl;
 		request_.Clear();
