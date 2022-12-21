b71f1ba3d508ee7155cb536a146d616532473758
maindishes <65796820+maindishes@users.noreply.github.com>
2022-12-21 19:42:36 +0900

    feat: use ParseUse function in main

--- app/main.cpp
+++ app/main.cpp
@@ -7,8 +7,10 @@ int main(int argc, char **argv) {
 	std::string config;
 	Webserv webserv;
 	std::vector<Server> server_blocks;
+	typedef std::map<std::string, std::vector<Server> > use_type;
+	use_type use_map;
 
-	if (argc > 2) {
+	if (argc > 2) { 
 		std::cerr << "Usage: ./webserv [config]" << std::endl;
 		return EXIT_FAILURE;
 	}
@@ -21,9 +23,8 @@ int main(int argc, char **argv) {
 		ConfigParser config_parser(config.c_str());
 		config_parser.Parse(server_blocks);
 		config_parser.PrintConf(server_blocks);
+		config_parser.ParseUse(use_map, server_blocks);
 
-		webserv.SetupServer(server_blocks);
-		webserv.StartServer();
 	} catch (const std::exception &e) {
 		std::cerr << e.what() << '\n';
 	}
