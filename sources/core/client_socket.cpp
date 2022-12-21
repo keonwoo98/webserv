0165b331c68b3e4899bc0c0f3803120af3a6a7e7
u-lo-l <jeffk0408@naver.com>
2022-12-22 03:58:09 +0900

    chore: clientSocket객체에서 request_parser_ 빼기 시작

--- sources/core/client_socket.cpp
+++ sources/core/client_socket.cpp
@@ -1,109 +1,27 @@
 #include "client_socket.hpp"
+#include "character_const.hpp"
 
-ClientSocket::ClientSocket(int sock_d, const ServerInfo &server_info) : Socket(server_info, Socket::CLIENT_TYPE, sock_d) {
+ClientSocket::ClientSocket(int sock_d, const ServerInfo &server_info)
+  : Socket(server_info, Socket::CLIENT_TYPE, sock_d), 
+	request_(server_info_.GetClientMaxBodySize()) {}
+
+ClientSocket::~ClientSocket() {}
+
+void ClientSocket::RecvRequest() {
+	char tmp[BUFFER_SIZE];
+	int n = recv(sock_d_, tmp, sizeof(tmp), 0);
+	if (n <= 0) {
+		std::cerr << "recv error" << std::endl;
+		exit(1);
+	} else {
+		tmp[n] = '\0';
+		// std::cout << C_RED << "[ClientSocket::RecvRequest] : recv line -> ";
+		// std::cout << tmp << C_RESET << std::endl;
+		request_parser_(request_, tmp);
+	}
+	if (request_.GetState() == DONE) {
+		std::cout << C_CYAN << "[ClientSocket::RecvRequest] : Parsing Done. result :" << C_RESET << std::endl;
+		std::cout << request_ << std::endl;
+		request_.Clear();
+	}
 }
-
-ClientSocket::~ClientSocket() {
-}
-
-//ClientSocket::ClientSocket(int sock_d, const Server &server_info)
-//	: Socket(server_info, Socket::CLIENT_TYPE, sock_d),
-//	  request_(server_info.GetClientMaxBodySize()),
-//	  parser_(request_),
-//	  response_(request_),
-//	  prev_state_(INIT),
-//	  state_(REQUEST) {
-
-//const int ClientSocket::BUFFER_SIZE = 1024;
-
-//}
-
-//const ClientSocket::State &ClientSocket::GetPrevState() const {
-//	return prev_state_;
-//}
-//
-//void ClientSocket::SetPrevState(const ClientSocket::State &prev_state) {
-//	prev_state_ = prev_state;
-//}
-//
-//const ClientSocket::State &ClientSocket::GetState() const { return state_; }
-//
-//void ClientSocket::SetState(const ClientSocket::State &state) {
-//	state_ = state;
-//}
-
-//const int &ClientSocket::GetFileDescriptor() const { return file_d_; }
-//
-//void ClientSocket::SetFileDescriptor(const int &file_d) { file_d_ = file_d; }
-//
-//bool ClientSocket::IsStateChanged() const { return prev_state_ != state_; }
-//
-//void ClientSocket::PrintRequest() const { std::cout << parser_; }
-//
-//void ClientSocket::RecvRequest() {
-//	prev_state_ = state_;
-//	char tmp[BUFFER_SIZE];
-//	int n = recv(sock_d_, tmp, sizeof(tmp), 0);
-//	if (n <= 0) {
-//		std::cerr << "recv error" << std::endl;
-//	} else {
-//		tmp[n] = '\0';
-//		// std::cout << tmp << std::endl; // for debugging
-//		parser_.AppendMessage(tmp);
-//	}
-//	if (parser_.IsDone()) {
-////		OpenFile(O_RDONLY);
-//		ChangeState(READ_FILE);
-//	}
-//}
-
-//void ClientSocket::OpenFile(int mode) {
-//	Uri uri(request_.GetUri());
-//	file_d_ = open(uri.GetPath().c_str(), mode);
-//	std::cout << uri.GetPath() << std::endl;
-//	if (file_d_ < 0) {
-//		perror("open error");
-//	}
-//	if (fcntl(file_d_, F_SETFL, O_NONBLOCK) < 0) {
-//		perror("fcntl error");
-//	}
-//}
-
-//void ClientSocket::ReadFile(intptr_t data) {
-//	char tmp[BUFFER_SIZE];
-//	prev_state_ = state_;
-//	int n = read(file_d_, tmp, sizeof(tmp));
-//	if (n < 0) {
-//		std::cerr << "read error" << std::endl;
-//		// throw HTTP_EXCEPTION(400);
-//	} else {
-//		tmp[n] = '\0';
-////		response_.AppendBody(tmp);
-//	}
-//	if (n < 0 || data <= n) {
-//		close(file_d_);
-//		ChangeState(RESPONSE);
-//	}
-//}
-
-//void ClientSocket::SendResponse() {
-//	PrintRequest(); // for debugging
-//	prev_state_ = state_;
-//	response_.CreateMessage();
-//	buffer_ = response_.GetMessage();
-//	send(sock_d_, buffer_.c_str(), buffer_.length(), 0);
-//	ChangeState(REQUEST);
-//	ResetSocket();
-//}
-
-//void ClientSocket::ChangeState(ClientSocket::State state) {
-//	prev_state_ = state_;
-//	state_ = state;
-//}
-//
-//void ClientSocket::ResetSocket() {
-//	buffer_.clear();
-//	parser_.Reset();
-//	request_.Clear();
-////	response_.Clear();
-//}
