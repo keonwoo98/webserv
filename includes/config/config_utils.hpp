54a3dc264328f651c4043aca9c1cd329de526585
keonwoo98 <keonwoo98@naver.com>
2022-12-27 20:33:43 +0900

    refactor: 주석 제거, indent 수정

--- includes/config/config_utils.hpp
+++ includes/config/config_utils.hpp
@@ -3,11 +3,14 @@
 
 #include <string>
 #include <vector>
-#include "server_info.hpp"
+
 #include "location_info.hpp"
+#include "server_info.hpp"
 
 std::vector<std::string> Split(std::string input, char delimiter);
-ServerInfo FindServerInfoToRequestHost(const std::string &server_name, const std::vector<ServerInfo> &ServerInfo_);
-int FindLocationInfoToUri(const std::string &uri, const ServerInfo &ServerInfo_);
+ServerInfo FindServerInfoToRequestHost(
+	const std::string &server_name, const std::vector<ServerInfo> &ServerInfo_);
+int FindLocationInfoToUri(const std::string &uri,
+						  const ServerInfo &ServerInfo_);
 
 #endif
