7299218472b1148e669adc188af16b6a303b2509
u-lo-l <jeffk0408@naver.com>
2022-12-27 17:48:30 +0900

    refactor : HttpException클래스 생성자 및 멤버 수정

--- sources/exception/http_exception.cpp
+++ sources/exception/http_exception.cpp
@@ -1,33 +1,15 @@
 #include "http_exception.hpp"
 #include "status_code.hpp"
 
-HttpException::HttpException(int status_code) : status_code_(status_code) {}
+HttpException::HttpException(int status_code, const std::string &message)
+: status_code_(status_code) {
+	err_msg_ = std::to_string(status_code) + " | " + message;
+}
+
+HttpException::~HttpException() throw() {}
 
 const char *HttpException::what() const throw() {
-	if (status_code_ == BAD_REQUEST)
-		return ("400: Bad Request");
-	else if (status_code_ == FORBIDDEN)
-		return ("403 : Forbidden");
-	else if (status_code_ == NOT_FOUND)
-		return ("404 : Not Found");
-	else if (status_code_ == METHOD_NOT_ALLOWED)
-		return ("405 : Method not allowed");
-	else if (status_code_ == NOT_ACCEPTABLE)
-		return ("406 : Not acceptable");
-	else if (status_code_ == LENGTH_REQUIRED)
-		return ("411 : Length Required");
-	else if (status_code_ == PAYLOAD_TOO_LARGE)
-		return ("413 : Payload too large");
-	else if (status_code_ == URI_TOO_LONG)
-		return ("414 : URI too long");
-	else if (status_code_ == INTERNAL_SERVER_ERROR)
-		return ("500 : Internal server error");
-	else if (status_code_ == NOT_IMPLEMENTED)
-		return ("501 : Not Implemented");
-	else if (status_code_ == HTTP_VERSION_NOT_SUPPORTED)
-		return ("505 : HTTP version not supported");
-	else
-		return ("Another Status");
+	return (err_msg_.c_str());
 }
 
 int HttpException::GetStatusCode() const {
