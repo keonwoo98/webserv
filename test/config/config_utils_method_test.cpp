0df1a42d3ac9bc6ee641a83e348db3e0259048e1
maindishes <65796820+maindishes@users.noreply.github.com>
2022-12-28 22:00:24 +0900

    chore: add TEST

--- test/config/config_utils_method_test.cpp
+++ test/config/config_utils_method_test.cpp
@@ -5,59 +5,74 @@
 
 
 namespace {
-    // 1's server block
-    TEST(ser, outputtest) {
+    // // 1's server block
+    // TEST(ser, outputtest) {
+    //     // required variables
+    //     std::vector<ServerInfo> server_blocks;
+    //     ConfigParser::server_configs_type use_map;
+
+    //     // setting value
+    //     std::string config("./conf/example.conf");
+    //     // std::string test_uri("/index.html");
+    //     ServerInfo outp;
+
+    //     // config parsing
+    //     ConfigParser config_parser(config.c_str());
+    //     config_parser.Parse(server_blocks);
+    //     config_parser.ParseConfigs(use_map, server_blocks);
+
+    //     // test: (uri: `/`) 1번째 host: 1번째 server_info: location 없을 경우
+    //     outp = FindServerInfoToRequestHost("localhost", use_map.find("0.0.0.0:4343")->second);
+    //     EXPECT_EQ(outp.GetClientMaxBodySize(), server_blocks[0].GetClientMaxBodySize());
+    // }
+    // TEST(ser, outputtest2) {
+    //     // required variables
+    //     std::vector<ServerInfo> server_blocks;
+    //     ConfigParser::server_configs_type use_map;
+
+    //     // setting value
+    //     std::string config("./conf/example.conf");
+    //     // std::string test_uri("/index.html");
+    //     ServerInfo outp;
+
+    //     // config parsing
+    //     ConfigParser config_parser(config.c_str());
+    //     config_parser.Parse(server_blocks);
+    //     config_parser.ParseConfigs(use_map, server_blocks);
+
+    //     // test: (uri: `/`) 1번째 host: 1번째 server_info: location 없을 경우
+    //     outp = FindServerInfoToRequestHost("localhost", use_map.find("0.0.0.0:4343")->second);
+    //     // EXPECT_EQ(outp.GetClientMaxBodySize(), server_blocks[0].GetClientMaxBodySize());
+    //     int test = FindLocationInfoToUri("/a/b/c", outp);
+    //     EXPECT_EQ(-1, test);
+    // }
+    // TEST(config_test, redirect_test) {
+    //     // required variables
+    //     std::vector<ServerInfo> server_blocks;
+    //     ConfigParser::server_configs_type use_map;
+
+    //     // setting value
+    //     std::string config("./conf/example.conf");
+    //     // config parsing
+    //     ConfigParser config_parser(config.c_str());
+    //     config_parser.Parse(server_blocks);
+    //     config_parser.ParseConfigs(use_map, server_blocks);
+    //     EXPECT_EQ("/test/index.html", server_blocks[0].GetLocations()[0].GetRedirect());
+    //     // test: (uri: `/`) 1번째 host: 1번째 server_info: location 없을 경우
+
+    // }
+    TEST(config_test, cgi_in_serverinfo) {
         // required variables
         std::vector<ServerInfo> server_blocks;
         ConfigParser::server_configs_type use_map;
 
         // setting value
         std::string config("./conf/example.conf");
-        // std::string test_uri("/index.html");
-        ServerInfo outp;
-
         // config parsing
         ConfigParser config_parser(config.c_str());
         config_parser.Parse(server_blocks);
         config_parser.ParseConfigs(use_map, server_blocks);
-
-        // test: (uri: `/`) 1번째 host: 1번째 server_info: location 없을 경우
-        outp = FindServerInfoToRequestHost("localhost", use_map.find("0.0.0.0:4343")->second);
-        EXPECT_EQ(outp.GetClientMaxBodySize(), server_blocks[0].GetClientMaxBodySize());
-    }
-    TEST(ser, outputtest2) {
-        // required variables
-        std::vector<ServerInfo> server_blocks;
-        ConfigParser::server_configs_type use_map;
-
-        // setting value
-        std::string config("./conf/example.conf");
-        // std::string test_uri("/index.html");
-        ServerInfo outp;
-
-        // config parsing
-        ConfigParser config_parser(config.c_str());
-        config_parser.Parse(server_blocks);
-        config_parser.ParseConfigs(use_map, server_blocks);
-
-        // test: (uri: `/`) 1번째 host: 1번째 server_info: location 없을 경우
-        outp = FindServerInfoToRequestHost("localhost", use_map.find("0.0.0.0:4343")->second);
-        // EXPECT_EQ(outp.GetClientMaxBodySize(), server_blocks[0].GetClientMaxBodySize());
-        int test = FindLocationInfoToUri("/a/b/c", outp);
-        EXPECT_EQ(-1, test);
-    }
-    TEST(config_test, redirect_test) {
-        // required variables
-        std::vector<ServerInfo> server_blocks;
-        ConfigParser::server_configs_type use_map;
-
-        // setting value
-        std::string config("./conf/example.conf");
-        // config parsing
-        ConfigParser config_parser(config.c_str());
-        config_parser.Parse(server_blocks);
-        config_parser.ParseConfigs(use_map, server_blocks);
-        EXPECT_EQ("/test/index.html", server_blocks[0].GetLocations()[0].GetRedirect());
+        EXPECT_EQ(".php", server_blocks[0].GetCgi()[0]);
         // test: (uri: `/`) 1번째 host: 1번째 server_info: location 없을 경우
 
     }
