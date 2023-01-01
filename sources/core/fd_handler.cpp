b6a37487083f3e91922395d3cd96054e7816bcf3
Minjune <minjune8506@gmail.com>
2023-01-01 14:24:34 +0900

    refactor: open file throw 리팩토링
    - HttpException 사용하도록 변경
    - perror 대신에 http exception에 에러 메시지 추가

--- sources/core/fd_handler.cpp
+++ sources/core/fd_handler.cpp
@@ -2,22 +2,20 @@
 // Created by Alvin Lee on 2022/12/25.
 //
 
-#include <errno.h>
 #include <fcntl.h>
+#include <cerrno>
 #include "fd_handler.hpp"
-#include "client_socket.hpp"
+#include "http_exception.hpp"
 #include "udata.hpp"
 
 int OpenFile(const Udata *user_data) {
 	int fd = open(user_data->request_message_.GetResolvedUri().c_str(), O_RDONLY);
     if (fd < 0) {
         if (errno == ENOENT) {
-            std::perror("open: NOT_FOUND");
-            throw (HTTP_EXCEPTION(NOT_FOUND));
+            throw HttpException(NOT_FOUND, "OpenFile open() NOT_FOUND");
         }
         if (errno == EACCES) {
-            std::perror("open: FORBIDDEN");
-            throw (HTTP_EXCEPTION(FORBIDDEN));
+            throw HttpException(FORBIDDEN, "OpenFile open() FORBIDDEN");
         }
     }
 	return fd;
