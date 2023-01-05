953e7226b8e3b5514acd2516fd556d4307f8944f
u-lo-l <jeffk0408@naver.com>
2023-01-05 10:08:35 +0900

    fix:  chunked일 때 매 번 사이즈 체크하는 부분 제거
    parse done 이후에 체크하도록 함.

--- sources/request/request_chunked_parser.cpp
+++ sources/request/request_chunked_parser.cpp
@@ -132,15 +132,6 @@ static RequestState ChunkSizeCRLF(RequestMessage &req_msg, char c) {
 		if (chunk_size == 0)
 			return BODY_CHUNK_LASTDATA;	 // -> 0 혹은 extension 이후에 CRLF를
 										 // 만나면 LASTDATA로 간다.
-		if (chunk_size + req_msg.GetChunkSize() >
-			req_msg.GetClientMaxBodySize()) {
-			std::string err_msg =
-				"(chunked parser) : max client body size is " +
-				int_to_str(req_msg.GetClientMaxBodySize()) +
-				". input size is " +
-				int_to_str(chunk_size + req_msg.GetChunkSize());
-			throw HttpException(PAYLOAD_TOO_LARGE, err_msg.c_str());
-		}
 		req_msg.SetChunkSize(chunk_size);
 		return BODY_CHUNK_DATA;
 	}
@@ -185,7 +176,6 @@ static RequestState ChunkData(RequestMessage &req_msg, char c) {
 		if (c == CR)
 			return BODY_CHUNK_CRLF;
 		else  // RFC에 사이즈와 CLRF가 다르면 어떨 지 안나와있다. 일단
-			  // 에러처리함.
 			throw HttpException(BAD_REQUEST,
 								"(chunked parser) : chunk size differs to "
 								"actual chunk body size");
@@ -221,8 +211,6 @@ static RequestState ChunkCRLF(RequestMessage &req_msg, char c) {
 		req_msg.ClearChunkSize();
 		req_msg.ClearChunkSizeStr();
 		req_msg.ClaerChunkBody();
-		// std::cout << C_YELLOW << "new size: " << req_msg.GetBody().size() <<
-		// std::endl;
 		return BODY_CHUNK_START;
 	} else
 		throw HttpException(BAD_REQUEST,
